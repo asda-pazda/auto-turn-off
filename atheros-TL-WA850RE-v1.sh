a238b276a343a8a22aec66a9670ab4ed45b207a4
asda-pazda <superuser@linux.pl>
2016-03-12 19:11:15 +0100

    Optimalization

--- atheros-TL-WA850RE-v1.sh
+++ atheros-TL-WA850RE-v1.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 disabled='0'
 while [ $disabled == '0' ]
+echo timer > /sys/class/leds/tp-link\:blue\:signal5/trigger
+echo timer > /sys/class/leds/tp-link\:blue\:signal4/trigger
 do
 	if [ $( arp -n | wc -l ) == 1 ]
 	then
@@ -9,8 +11,6 @@ do
 		wifi down #Shutting down radio, nothing connected
 		disabled='1'
 	else
-		echo timer > /sys/class/leds/tp-link\:blue\:signal5/trigger
-		echo timer > /sys/class/leds/tp-link\:blue\:signal4/trigger
 		echo 2000 > /sys/class/leds/tp-link\:blue\:signal5/delay_on
 		echo 2500 > /sys/class/leds/tp-link\:blue\:signal4/delay_on
 		echo 1900 > /sys/class/leds/tp-link\:blue\:signal5/delay_off
