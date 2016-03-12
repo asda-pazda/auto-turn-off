fdf8b9384b2f7e2a7718f56300c372beeb9f12f9
asda-pazda <superuser@linux.pl>
2016-03-12 19:09:14 +0100

    Infinite loop fix, fancy LEDs when something still connected

--- atheros-TL-WA850RE-v1.sh
+++ atheros-TL-WA850RE-v1.sh
@@ -4,12 +4,16 @@ while [ $disabled == '0' ]
 do
 	if [ $( arp -n | wc -l ) == 1 ]
 	then
+		echo none > /sys/class/leds/tp-link\:blue\:signal5/trigger
+		echo none > /sys/class/leds/tp-link\:blue\:signal4/trigger
 		wifi down #Shutting down radio, nothing connected
+		disabled='1'
 	else
-		echo 255 > /sys/class/leds/tp-link\:blue\:signal5/brightness
-		echo 0 > /sys/class/leds/tp-link\:blue\:signal5/brightness
-		echo 255 > /sys/class/leds/tp-link\:blue\:signal4/brightness
-		echo 0 > /sys/class/leds/tp-link\:blue\:signal4/brightness
-		sleep 1
+		echo timer > /sys/class/leds/tp-link\:blue\:signal5/trigger
+		echo timer > /sys/class/leds/tp-link\:blue\:signal4/trigger
+		echo 2000 > /sys/class/leds/tp-link\:blue\:signal5/delay_on
+		echo 2500 > /sys/class/leds/tp-link\:blue\:signal4/delay_on
+		echo 1900 > /sys/class/leds/tp-link\:blue\:signal5/delay_off
+		echo 2400 > /sys/class/leds/tp-link\:blue\:signal4/delay_off
 	fi;
 done
